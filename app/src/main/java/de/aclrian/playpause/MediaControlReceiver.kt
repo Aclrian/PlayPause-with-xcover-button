234cc120d99c4ea83da884aa488c9cc87940171e
Aclrian <32142988+Aclrian@users.noreply.github.com>
2023-11-06 19:41:46 +0100

    add haptic feedback

--- app/src/main/java/de/aclrian/playpause/MediaControlReceiver.kt
+++ app/src/main/java/de/aclrian/playpause/MediaControlReceiver.kt
@@ -1,5 +1,6 @@
 package de.aclrian.playpause
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -7,10 +8,13 @@ import android.media.AudioDeviceInfo
 import android.media.AudioManager
 import android.os.Looper
 import android.os.SystemClock
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.text.Html
 import android.util.Log
 import android.view.KeyEvent
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.text.HtmlCompat
 import java.util.*
 
@@ -22,26 +26,72 @@ private const val BUTTON_UP = 2
 class MediaControlReceiver : BroadcastReceiver() {
     private var startTime = -1L
 
-    enum class Duration {
-        SHORT, MEDIUM, LONG, EXTRA_LONG
+    /**
+    If noReplay is true, the previous button will be pressed
+    two times to assure that the previous song will be played
+    instead of a replay of the current song from the beginning.
+    Else, the default behavior of KeyEvent.KEYCODE_MEDIA_PREVIOUS will be executed
+     **/
+    val noReplay = true
+
+
+    enum class Duration(val time: Long) {
+        SHORT(1000L), MEDIUM(2000L), LONG(3000L), EXTRA_LONG(4000L)
     }
 
 
     override fun onReceive(context: Context, intent: Intent) {
         val keyCode = intent.getIntExtra(BUTTON_TYPE, -BUTTON_DOWN)
         val keyReportType = intent.getIntExtra(IS_BUTTON_PRESSED, -BUTTON_DOWN)
+
         if (keyCode == 1015) {
             if (keyReportType == BUTTON_DOWN) {
                 startTime = SystemClock.uptimeMillis()
-            }
-            else if (keyReportType == BUTTON_UP && startTime > 0) {
+                val notificationManager =
+                    context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                val audioManager =
+                    context.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+                if (notificationManager.currentInterruptionFilter == 1 && audioManager.ringerMode == 2 && !isInLoudspeakerMode(
+                        audioManager
+                    )
+                ) {
+                    Thread(
+                        Runnable {
+                            Thread.sleep(Duration.SHORT.time)
+                            if (startTime < 0) {
+                                return@Runnable
+                            }
+                            getSystemService(
+                                context,
+                                Vibrator::class.java
+                            )?.vibrate(VibrationEffect.createOneShot(100, 1))
+                            Thread.sleep(Duration.MEDIUM.time - 100 - Duration.SHORT.time)
+                            if (startTime < 0) {
+                                return@Runnable
+                            }
+                            getSystemService(
+                                context,
+                                Vibrator::class.java
+                            )?.vibrate(VibrationEffect.createOneShot(100, 1))
+                            Thread.sleep(Duration.LONG.time - 100 - Duration.MEDIUM.time)
+                            if (startTime < 0) {
+                                return@Runnable
+                            }
+                            getSystemService(
+                                context,
+                                Vibrator::class.java
+                            )?.vibrate(VibrationEffect.createOneShot(100, 1))
+                            Thread.sleep(Duration.MEDIUM.time - 100 - Duration.SHORT.time)
+                        }).start()
+                }
+            } else if (keyReportType == BUTTON_UP && startTime > 0) {
                 val endTime = SystemClock.uptimeMillis()
                 val duration = endTime - startTime
-                if (duration < 1000L) {
+                if (duration < Duration.SHORT.time) {
                     Thread { Looper.prepare(); act(context, Duration.SHORT) }.start()
-                } else if (duration < 3000L) {
+                } else if (duration < Duration.MEDIUM.time) {
                     Thread { Looper.prepare(); act(context, Duration.MEDIUM) }.start()
-                } else if (duration < 6000L) {
+                } else if (duration < Duration.LONG.time) {
                     Thread { Looper.prepare(); act(context, Duration.LONG) }.start()
                 } else {
                     Thread { Looper.prepare(); act(context, Duration.EXTRA_LONG) }.start()
@@ -51,12 +101,7 @@ class MediaControlReceiver : BroadcastReceiver() {
         }
     }
 
-    private fun act(context: Context, duration: Duration) {
-        Log.i("PlayPause", "act " + duration.name)
-        val audioManager =
-            context.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val eventTime = SystemClock.uptimeMillis()
-
+    private fun isInLoudspeakerMode(audioManager: AudioManager): Boolean {
         // check if the button press was intended -> should the action be performed?
         val isHeadsetConnected =
             Arrays.stream(audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS))
@@ -64,7 +109,15 @@ class MediaControlReceiver : BroadcastReceiver() {
                     it.type != AudioDeviceInfo.TYPE_BUILTIN_SPEAKER
                             && it.type != AudioDeviceInfo.TYPE_BUILTIN_EARPIECE
                 }
-        if (isHeadsetConnected || audioManager.availableCommunicationDevices.size == 0) {
+        return isHeadsetConnected || audioManager.availableCommunicationDevices.size == 0
+    }
+
+    private fun act(context: Context, duration: Duration) {
+        Log.i("PlayPause", "act " + duration.name)
+        val audioManager =
+            context.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val eventTime = SystemClock.uptimeMillis()
+        if (isInLoudspeakerMode(audioManager)) {
             Toast.makeText(context, "skipped", Toast.LENGTH_SHORT).show()
         } else {
             when (duration) {
@@ -109,6 +162,11 @@ class MediaControlReceiver : BroadcastReceiver() {
             0
         )
         audioManager.dispatchMediaKeyEvent(prevUpEvent)
+        if (noReplay) {
+            Thread.sleep(0, 1)
+            audioManager.dispatchMediaKeyEvent(prevDownEvent)
+            audioManager.dispatchMediaKeyEvent(prevUpEvent)
+        }
     }
 
     private fun pressNextSongButton(event_time: Long, audioManager: AudioManager) {
